34187137870bba1af23036ab7ab55354865b6dd9
Lina Duko <101510056+linaduko@users.noreply.github.com>
2022-07-17 18:15:06 +0300

    Update start_wpproject.sh

--- start_wpproject.sh
+++ start_wpproject.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #daem prava na vipolnenie
-chmod +x wplamp.bash
-chmod +x cron.sh
-chmod +x backupdb.sh
+chmod +x wplamp.bash cron.sh backupdb.sh
 #vperda
 . ./wplamp.bash && . ./cron.sh
 echo The cake is a lie.
