fb79ab62bd03ace1aa163d88d74740adaed02137
Stas Ziborov <zbrv91@gmail.com>
2022-07-17 15:15:09 +0300

    popravki dlya planirovshika

--- press_F_to_pay_respect.sh
+++ press_F_to_pay_respect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#4ernovik (vozmozhno polnaya fignya)
 #daem prava na vipolnenie
 chmod +x wplamp.bash
+chmod +x cron.sh
 chmod +x backupdb.sh
 #vperda
-. ./wplamp.bash && . ./backupdb.sh
+. ./wplamp.bash && . ./cron.sh
 echo The cake is a lie.
