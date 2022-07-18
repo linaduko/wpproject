d342f60698ce6d044b4ea6cb98bdec3f8eef7b85
Lina Duko <101510056+linaduko@users.noreply.github.com>
2022-07-18 11:15:04 +0300

    Delete start_wpproject.bash

--- start_wpproject.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Установка флага исполнения на файлы скриптов
-chmod +x wplamp.bash backupsqldb.bash crondaily.sh
-
-#Создание отношений между файлами скриптов
-. ./wplamp.bash && . ./backupsqldb.bash && . ./crondaily.bash
