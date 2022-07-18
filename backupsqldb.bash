714223a0170d2689ea78ff38a3405f977292c5d6
Lina <lina.duko@gmail.com>
2022-07-18 03:09:01 +0300

    Added comments into file

--- backupsqldb.bash
+++ backupsqldb.bash
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#actualdir=$(pwd)
-#. $actualdir/wplamp.bash
-
-echo $userdb $userpass $newdb
+#Создание директорий для хранения бэкапов
 mkdir $HOME/backup
-#sudo chown -R $USER:$USER $HOME/backup
-#sudo chmod -R 664 $HOME/backup
+
 dirforbackups=$(echo "$HOME/backup")
 datename=$(date +%F--%H-%M)
-#mysqldump -usadmindb -pAa123456 wordpress > $dirforbackups/$datename-new.sql
+
+#Создание бэкапа базы данных wordpress и перенаправление вывода в хранилище
 sudo mysqldump -u$userdb -p$userpass $newdb > $dirforbackups/$datename.sql
 
--- start_wpproject.bash
+++ start_wpproject.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
+
+#Установка флага исполнения на файлы скриптов
 chmod +x wplamp.bash backupsqldb.bash crondaily.sh
 
+#Создание отношений между файлами скриптов
 . ./wplamp.bash && . ./backupsqldb.bash && . ./crondaily.bash
-echo OK 
