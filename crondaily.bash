d4caf8e708328eeca8eb7c81dd350d36c92913b1
Lina Duko <101510056+linaduko@users.noreply.github.com>
2022-07-18 11:14:56 +0300

    Delete crondaily.bash

--- crondaily.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Установка Cron - демона для выполнения задач по расписанию
-sudo apt install cron
-sudo systemctl enable cron
-
-#Копирование бэкап-скрипта в директорию /etc/cron.daily для ежедневного выполнения
-sudo cp ./backupsqldb.bash /etc/cron.daily
